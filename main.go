7ce049601eca7af3072691e8459c829e34230d5c
Tom Dudley <git@tomdudley.io>
2025-12-09 16:51:59 +0000

    track decode index

--- main.go
+++ main.go
@@ -3,18 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	s := []byte{65, 66, 67, 0, 3, 2, 68, 69, 70}
-	for i := 0; i < len(s); i++ {
-		if s[i] == 0 {
-			lookback := s[i+1]
-			length := s[i+2]
+	// s := []byte{65, 66, 67, 0, 3, 2, 68, 69, 70}
+	encoded := []byte{65, 66, 0, 2, 2, 0, 2, 2}
+	decoded := []byte{}
+	decodedIndex := 0
+
+	for i := 0; i < len(encoded); i++ {
+		if encoded[i] == 0 {
+			lookback := encoded[i+1]
+			length := encoded[i+2]
 			for j := 0; j < int(length); j++ {
-				char := s[i-int(lookback)+j]
-				fmt.Println(string(char))
+				char := decoded[decodedIndex-int(lookback)]
+				decoded = append(decoded, char)
+				decodedIndex++
 			}
 			i += 2
 		} else {
-			fmt.Println(string(s[i]))
+			decoded = append(decoded, encoded[i])
+			decodedIndex++
 		}
 	}
+
+	fmt.Println(string(decoded))
 }
