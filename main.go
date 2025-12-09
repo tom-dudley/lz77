198a12d5f9144cb678e20e8884b180a4d0dbdd51
Tom Dudley <git@tomdudley.io>
2025-12-09 17:55:31 +0000

    fix byte size checking

--- main.go
+++ main.go
@@ -1,10 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non condimentum felis, ut viverra lorem. Cras laoreet felis non mi egestas, quis vestibulum turpis bibendum. Vivamus ex leo, cursus euismod leo eget, cursus condimentum velit. Sed aliquet, augue in gravida cursus, urna magna lobortis est, in lobortis orci ligula in lorem. Curabitur fringilla consequat mauris in elementum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse semper, ligula tempor gravida commodo, nulla mauris mollis metus, a fermentum dolor justo sed arcu. Nam vel varius erat. Sed vehicula aliquet magna. Sed facilisis sem sed ligula tristique mollis."
 
 func main() {
+	fmt.Println("Encoding...")
+	fmt.Printf("Input bytes: %d\n", len([]byte(lorem)))
+	encoded := encode([]byte(lorem))
+	fmt.Printf("Encoded bytes: %d\n", len(encoded))
+	fmt.Println("Decoding...")
 	// s := []byte{65, 66, 67, 0, 3, 2, 68, 69, 70}
-	encoded := []byte{65, 66, 0, 2, 2, 0, 2, 2}
+	// encoded := []byte{65, 66, 0, 2, 2, 0, 2, 2}
+	decoded := decode(encoded)
+	fmt.Println(string(decoded))
+}
+
+func encode(input []byte) []byte {
+	encoded := []byte{}
+	for i := 0; i < len(input); i++ {
+		lengths := []int{}
+		for j := 0; j < i; j++ {
+			length := 0
+			for {
+				if j+length == len(input) || i+length == len(input) {
+					lengths = append(lengths, length)
+					break
+				}
+				if input[j+length] == input[i+length] {
+					length++
+				} else {
+					lengths = append(lengths, length)
+					break
+				}
+			}
+		}
+		maxLength := 0
+		maxLengthIndex := -1
+		for k, length := range lengths {
+			// We only allow lengths up to 255 (1 byte per length)
+			if i-k > 255 {
+				break
+			}
+			if length > maxLength && length < 255 {
+				maxLength = length
+				maxLengthIndex = k
+			}
+		}
+
+		if maxLength > 3 {
+			encoded = append(encoded, 0x0)
+			encoded = append(encoded, byte(i-maxLengthIndex))
+			encoded = append(encoded, byte(maxLength))
+			i += maxLength - 1
+
+		} else {
+			encoded = append(encoded, input[i])
+		}
+	}
+
+	return encoded
+}
+
+func decode(encoded []byte) []byte {
 	decoded := []byte{}
 	decodedIndex := 0
 
@@ -23,6 +83,5 @@ func main() {
 			decodedIndex++
 		}
 	}
-
-	fmt.Println(string(decoded))
+	return decoded
 }
